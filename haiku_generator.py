904c288e788bae34f8528b2a45766575d008a107
WizardEchidna <wizardechidna@gmail.com>
2020-12-13 20:44:28 -0800

    Implement getting words from Datamuse API with random contexts with random words from a local list

--- haiku_generator.py
+++ haiku_generator.py
@@ -1,59 +1,69 @@
 import json
 import random
+import requests
 
-haiku_dictionary = ""
-uncouth_mode = False #whether or not to write foul and uncouth poetry
-def retrieve_dictionary():
-    if uncouth_mode:
-        with open('uncouth_dictionary.json', 'r') as haiku_dictionary_raw_json:
-            haiku_dictionary = json.loads(haiku_dictionary_raw_json.read())
-    else:
-        with open('haiku_dictionary.json', 'r') as haiku_dictionary_raw_json:
-            haiku_dictionary = json.loads(haiku_dictionary_raw_json.read())
+seed_list = []
+with open('random_seed_list.txt', 'r') as file:
+    seed_list = file.readlines()
 
 def generate_haiku():
     haiku = ""
-    for i in range(3):
+    for line in range(3):
         max_syllables = 5
-        if i == 1:
+        if line == 1:
             max_syllables = 7
         syllable_count = 0
         word_count = 0
         while syllable_count < max_syllables:
-            next_word = get_word();
-            if syllable_count + int(next_word['number_of_syllables']) <= max_syllables:
-                haiku += next_word['word'] + " "
-                syllable_count += int(next_word['number_of_syllables'])
+            random_word_list = get_random_word_list()
+            bad_entries = []
+            possible_word = choice_excluding(random_word_list, bad_entries)
+            if word_good(possible_word, max_syllables - syllable_count):
+                haiku += possible_word['word'] + " "
+                syllable_count += int(possible_word['numSyllables'])
                 word_count += 1
+            else:
+                bad_entries.append(possible_word)
         haiku += "\n"
     return haiku
 
-def get_word():
-    return haiku_dictionary['dictionary'][random.randint(0, len(haiku_dictionary['dictionary']) - 1)]
+def word_good(word_dict, max_syl):
+    frequency = float(word_dict['tags'][-1][2:])
+    is_valid_pos = True
+    for i in word_dict['tags']:
+        if i == 'prop' or i == 'u':
+            is_valid_pos = False
+    vowels = 'aeiouy'
+    has_vowel = False
+    for i in word_dict['word']:
+        if i in vowels:
+            has_vowel = True
+            break
+    if word_dict['numSyllables'] > max_syl or frequency < 1.0 or not is_valid_pos or not has_vowel:
+        return False
+    else:
+        return True
 
-def update_dictionary(word, part_of_speech, number_of_syllables):
-    dict_in_use = 'haiku_dictionary.json'
-    if uncouth_mode: dict_in_use = 'uncouth_dictionary.json'
-    haiku_dictionary_raw_json_temp = open(dict_in_use, 'r')
-    haiku_dictionary_raw_json = haiku_dictionary_raw_json_temp.read()
-    haiku_dictionary_raw_json_temp.close()
-    haiku_dictionary_dict = json.loads(haiku_dictionary_raw_json)
-    haiku_dictionary_dict['dictionary'].append({"word":word,"part_of_speech":part_of_speech,"number_of_syllables":number_of_syllables})
-    haiku_dictionary_string = json.dumps(haiku_dictionary_dict, indent=2)
-    with open(dict_in_use, 'w') as haiku_dictionary_temp:
-        haiku_dictionary_temp.write(haiku_dictionary_string)
-    with open(dict_in_use, 'r') as haiku_dictionary_temp:
-        haiku_dictionary = json.loads(haiku_dictionary_temp.read())
+def get_random_word_list():
+    source = "https://api.datamuse.com/words"
+    constraint_list = ['ml', 'sl','rel_ant', 'rel_nry','ml', 'ml', 'ml']
+    seed = random.choice(seed_list)
+    while True:
+        req = requests.get(source + '?' + random.choice(constraint_list) + '=' + seed + '&md=dpsrf').text
+        response = json.loads(req)
+        if len(response) != 0:
+            return response
 
-def dictionary_duplicate(word):
-    for i in haiku_dictionary['dictionary']:
-        if i['word'] == word:
-            return True
-    return False
+def choice_excluding(list, exclude):
+    revised_list = []
+    for item in list:
+        if item not in exclude:
+            revised_list.append(item);
+    return random.choice(revised_list)
 
-commands = "Commands: \nhaiku - generates a haiku\nadd word - add word to dictionary\nexit - close program\nhelp - \"remind me what the commands are, I'm a retard who can't remember 3 commands\""
+commands = "Commands: \nhaiku - generates a haiku\nexit - close program\nhelp - remind me what the commands are\n"
 
-print("Haiku Generator\nThis program generates pseudorandom haikus using its own ever-growing dictionary.")
+print("Haiku Generator\nThis program generates pseudorandom haikus using the Datamuse API.")
 print(commands)
 
 user_input = ""
@@ -63,22 +73,6 @@ while True:
         break
     elif user_input == "haiku":
         print(generate_haiku())
-        continue
-    elif user_input == "uncouth mode":
-        uncouth_mode = not uncouth_mode
-    elif user_input == "add word":
-        while True:
-            new_word = input("Enter your new word:\n")
-            if dictionary_duplicate(new_word):
-                print("This word is already in the dictionary :)")
-                continue
-            part_of_speech = input("What part of speech is your word? \nPossible parts of speech: noun, proper noun, pronoun, verb, adjective, adverb, preposition, conjunction, article\n")
-            number_of_syllables = input("How many syllables does your word have? Must be less than 7\n")
-            if int(number_of_syllables) > 7:
-                print("Too many syllables")
-                continue
-            update_dictionary(new_word, part_of_speech, number_of_syllables)
-            break
         continue
     elif user_input == "help":
         print(commands)
